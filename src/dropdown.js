3eb48457421e0adea052d1722102342663de4ebe
Simeon Rediger <simeonrediger@gmail.com>
2025-11-27 19:26:29 -0500

    Add missing early return statement

--- src/dropdown.js
+++ src/dropdown.js
@@ -14,6 +14,7 @@ function mapContentToButtons(buttons) {
 
         if (!nextElement.matches(contentSelector)) {
             handleWrongNextElement(button, nextElement);
+            return;
         }
 
         buttonsAndContent.set(button, nextElement);
