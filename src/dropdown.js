91067c32653bc5528fb9106bceee43622acfe9cf
Simeon Rediger <simeonrediger@gmail.com>
2025-11-27 19:00:40 -0500

    Map content to buttons

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,4 +1,38 @@
-function init(root = document) {}
+const buttonSelector = "[data-dropdown='button']";
+const contentSelector = "[data-dropdown='content']";
+
+function init(root = document) {
+    const buttons = root.querySelectorAll?.(buttonSelector);
+    const buttonsAndContent = mapContentToButtons(buttons);
+}
+
+function mapContentToButtons(buttons) {
+    const buttonsAndContent = new Map();
+
+    for (const button of buttons) {
+        const nextElement = button.nextElementSibling;
+
+        if (nextElement.matches(contentSelector)) {
+            buttonsAndContent.set(button, nextElement);
+        } else {
+            handleWrongNextElement(button, nextElement);
+        }
+    }
+
+    return buttonsAndContent;
+}
+
+function handleWrongNextElement(button, nextElement) {
+    console.error(
+        'Failed to initialize dropdown.',
+        `Element with the "${buttonSelector}" selector was not immediately`,
+        `followed by an element with the "${contentSelector}" selector.`,
+        '\n\nButton:',
+        button,
+        '\nNext Element:',
+        nextElement,
+    );
+}
 
 const dropdown = {
     init,
